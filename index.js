3deaf62588d59f63424bfd28fb658016983f74e9
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-17 16:26:13 -0300

    fiz bugs

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-const section = document.querySelector("._3Kxus").children[0];
+const interval = setInterval(() => {
+  const section = document.querySelector("._3Kxus").children[0];
+  if (section) {
+    clearInterval(interval);
 
-const buttonSlow = document.createElement("button");
-buttonSlow.innerHTML = "1x";
-buttonSlow.classList.add("change-speed");
-section.appendChild(buttonSlow);
+    const buttonSlow = document.createElement("button");
+    buttonSlow.innerHTML = "1x";
+    buttonSlow.classList.add("change-speed");
+    buttonSlow.classList.add("clicked");
+    section.appendChild(buttonSlow);
 
-const buttonMiddle = document.createElement("button");
-buttonMiddle.innerHTML = "1.5x";
-buttonMiddle.classList.add("change-speed");
-section.appendChild(buttonMiddle);
+    const buttonMiddle = document.createElement("button");
+    buttonMiddle.innerHTML = "1.5x";
+    buttonMiddle.classList.add("change-speed");
+    section.appendChild(buttonMiddle);
 
-buttonMiddle.addEventListener("click", () => {
-  const audiosToFast = document.querySelectorAll("audio");
-  for (audio of audiosToFast) {
-    audio.playbackRate = 1.5;
+    const allButtons = document.querySelectorAll("button.change-speed");
+
+    buttonMiddle.addEventListener("click", () => {
+      for (button of allButtons) {
+        button.classList.remove("clicked");
+      }
+      buttonMiddle.classList.add("clicked");
+      const audiosToFast = document.querySelectorAll("audio");
+      for (audio of audiosToFast) {
+        audio.playbackRate = 1.5;
+      }
+    });
+
+    buttonSlow.addEventListener("click", () => {
+      for (button of allButtons) {
+        button.classList.remove("clicked");
+      }
+      buttonSlow.classList.add("clicked");
+      const audiosToSlow = document.querySelectorAll("audio");
+      for (audio of audiosToSlow) {
+        audio.playbackRate = 1;
+      }
+    });
   }
-});
-
-buttonSlow.addEventListener("click", () => {
-  const audiosToSlow = document.querySelectorAll("audio");
-  for (audio of audiosToSlow) {
-    audio.playbackRate = 1;
-  }
-});
+}, 1000);
