87b7d51c39352fa2cc209068aae30bc6fee68b3d
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-17 17:58:06 -0300

    clean code and add 2x

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+function checkedButton(allButtons, mainButton) {
+  for (button of allButtons) {
+    button.classList.remove("clicked");
+  }
+  mainButton.classList.add("clicked");
+}
+
+function changeSpeed(speed) {
+  const audios = document.querySelectorAll("audio");
+
+  const methods = {
+    slow() {
+      for (audio of audios) {
+        audio.playbackRate = 1;
+      }
+    },
+    middle() {
+      for (audio of audios) {
+        audio.playbackRate = 1.5;
+      }
+    },
+    fast() {
+      for (audio of audios) {
+        audio.playbackRate = 2;
+      }
+    },
+  };
+  methods[speed]();
+}
+
 const interval = setInterval(() => {
   const section = document.querySelector("._3Kxus").children[0];
   if (section) {
@@ -14,28 +44,24 @@ const interval = setInterval(() => {
     buttonMiddle.classList.add("change-speed");
     section.appendChild(buttonMiddle);
 
+    const buttonFast = document.createElement("button");
+    buttonFast.innerHTML = "2x";
+    buttonFast.classList.add("change-speed");
+    section.appendChild(buttonFast);
+
     const allButtons = document.querySelectorAll("button.change-speed");
 
-    buttonMiddle.addEventListener("click", () => {
-      for (button of allButtons) {
-        button.classList.remove("clicked");
-      }
-      buttonMiddle.classList.add("clicked");
-      const audiosToFast = document.querySelectorAll("audio");
-      for (audio of audiosToFast) {
-        audio.playbackRate = 1.5;
-      }
-    });
-
     buttonSlow.addEventListener("click", () => {
-      for (button of allButtons) {
-        button.classList.remove("clicked");
-      }
-      buttonSlow.classList.add("clicked");
-      const audiosToSlow = document.querySelectorAll("audio");
-      for (audio of audiosToSlow) {
-        audio.playbackRate = 1;
-      }
+      checkedButton(allButtons, buttonSlow);
+      changeSpeed("slow");
+    });
+    buttonMiddle.addEventListener("click", () => {
+      checkedButton(allButtons, buttonMiddle);
+      changeSpeed("middle");
+    });
+    buttonFast.addEventListener("click", () => {
+      checkedButton(allButtons, buttonFast);
+      changeSpeed("fast");
     });
   }
 }, 1000);
