e2250665b384d85e48addbce77bc888e9eace04b
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-17 15:37:13 -0300

    add css class

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const section = document.querySelector("._3Kxus").children[0];
 
 const buttonSlow = document.createElement("button");
-section.appendChild(buttonSlow);
 buttonSlow.innerHTML = ">";
+buttonSlow.classList.add("change-speed");
+section.appendChild(buttonSlow);
 
 const buttonFast = document.createElement("button");
-section.appendChild(buttonFast);
 buttonFast.innerHTML = ">>";
+buttonFast.classList.add("change-speed");
+section.appendChild(buttonFast);
 
 buttonFast.addEventListener("click", () => {
   const audiosToFast = document.querySelectorAll("audio");
